d9daf04038cb0fb6c288d9e7b4a37b4342c5546b
39189058@qq.com <39189058@qq.com>
2018-07-02 21:47:03 +0800

    dbscript update

--- db/table.sql
+++ db/table.sql
@@ -26,7 +26,7 @@ CREATE TABLE `tcase` (
   `case_happen_address` varchar(100) NOT NULL COMMENT '案发地点',
   `project_code` varchar(32) NOT NULL COMMENT '案件所涉项目代码',
   `project_name` varchar(100) NOT NULL COMMENT '案件所涉项目名称',
-  `case_cause` varchar(500) NOT NULL COMMENT '案由',
+  `case_cause` varchar(200) NOT NULL COMMENT '案由',
   `case_summary` varchar(500) NOT NULL COMMENT '案情摘要',
   `case_handler` varchar(64) NOT NULL COMMENT '办案人',
   `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
@@ -34,7 +34,7 @@ CREATE TABLE `tcase` (
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
   `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
-  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记'
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='案件';
 
@@ -54,7 +54,7 @@ CREATE TABLE `tcase_attach` (
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
   `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
-  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记'
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='案件资料';
 
@@ -77,6 +77,145 @@ CREATE TABLE `tcase_notify` (
   `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
   `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
-  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记'
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='案件告知书';
+
+-- ----------------------------
+-- Table structure for tsitecheck
+-- ----------------------------
+DROP TABLE IF EXISTS `tsitecheck`;
+CREATE TABLE `tsitecheck` (
+  `id` int(11) NOT NULL auto_increment  COMMENT '编号',
+  `develop_org` varchar(100) NOT NULL COMMENT '建设单位',
+  `develop_contact` varchar(32) NOT NULL COMMENT '建设单位联系人',
+  `develop_phone` varchar(32) NOT NULL COMMENT '建设单位联系人电话',
+  `construction_org` varchar(100) NOT NULL COMMENT '施工单位',
+  `construction_contact` varchar(32) NOT NULL COMMENT '施工单位联系人',
+  `project_name` varchar(100) NOT NULL COMMENT '工程名称',
+  `project_address` varchar(100) NOT NULL COMMENT '工程地址',
+  `site_situation` varchar(500) NOT NULL COMMENT '现场检查工程情况',
+  `site_picture` varchar(500) NOT NULL COMMENT '现场踏勘示意图',
+  `site_check_result` varchar(500) NOT NULL COMMENT '现场踏勘情况',
+  `checker` varchar(32) NOT NULL COMMENT '勘查人',
+  `check_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '勘查时间',
+  `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='现场踏勘';
+
+-- ----------------------------
+-- Table structure for tsiterecord
+-- ----------------------------
+DROP TABLE IF EXISTS `tsiterecord`;
+CREATE TABLE `tsiterecord` (
+  `id` int(11) NOT NULL auto_increment  COMMENT '编号',
+  `develop_org` varchar(100) NOT NULL COMMENT '建设单位',
+  `develop_contact` varchar(32) NOT NULL COMMENT '建设单位联系人',
+  `develop_phone` varchar(32) NOT NULL COMMENT '建设单位联系人电话',
+  `construction_org` varchar(100) NOT NULL COMMENT '施工单位',
+  `construction_contact` varchar(32) NOT NULL COMMENT '施工单位联系人',
+  `project_name` varchar(100) NOT NULL COMMENT '工程名称',
+  `project_address` varchar(100) NOT NULL COMMENT '工程地址',
+  `site_situation` varchar(500) NOT NULL COMMENT '现场检查工程情况',
+  `site_picture` varchar(500) NOT NULL COMMENT '现场踏勘示意图',
+  `site_check_result` varchar(500) NOT NULL COMMENT '现场踏勘情况',
+  `checker` varchar(32) NOT NULL COMMENT '勘查人',
+  `check_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '勘查时间',
+  `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='现场检查笔录';
+
+-- ----------------------------
+-- Table structure for tquestionanswer
+-- ----------------------------
+DROP TABLE IF EXISTS `tquestionanswer`;
+CREATE TABLE `tquestionanswer` (
+  `id` int(11) NOT NULL auto_increment  COMMENT '编号',
+  `case_cause` varchar(200) NOT NULL COMMENT '案由',  
+  `from_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '开始时间',
+  `to_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '结束时间',
+  `location` varchar(100) NOT NULL COMMENT '地点',
+  `quizzer` varchar(32) DEFAULT NULL COMMENT '调查询问人',
+  `recorder` varchar(32) DEFAULT NULL COMMENT '记录人',
+  `answerer` varchar(32) DEFAULT NULL COMMENT '被询问人',
+  `answerer_sex` varchar(8) NOT NULL COMMENT '性别', 
+  `answerer_code` varchar(32) NOT NULL COMMENT '身份证',
+  `answerer_organization` varchar(100) NOT NULL COMMENT '工作单位',
+  `answerer_post` varchar(100) NOT NULL COMMENT '职务',
+  `answerer_birthday` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '出生年月',
+  `answerer_address` varchar(100) NOT NULL COMMENT '住址',
+  `answerer_phone` varchar(32) NOT NULL COMMENT '联系电话',   
+  `zip_code` varchar(32) NOT NULL COMMENT '身份证',
+  `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='询问笔录';
+
+-- ----------------------------
+-- Table structure for tquestionanswer_item
+-- ----------------------------
+DROP TABLE IF EXISTS `tquestionanswer_item`;
+CREATE TABLE `tquestionanswer_item` (
+  `id` int(11) NOT NULL auto_increment  COMMENT '编号',
+  `qa_id` int(11) NOT NULL COMMENT '询问笔录编号', 
+  `qa_content` varchar(500) NOT NULL COMMENT '问答',
+  `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='询问笔录项目';
+
+-- ----------------------------
+-- Table structure for tmaterial
+-- ----------------------------
+DROP TABLE IF EXISTS `tmaterial`;
+CREATE TABLE `tmaterial` (
+  `id` int(11) NOT NULL auto_increment  COMMENT '编号',
+  `get_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '收集时间',
+  `material_type` varchar(32) NOT NULL COMMENT '收集方式',
+  `get_location` varchar(100) NOT NULL COMMENT '收集地点',
+  `party_sig` varchar(32) DEFAULT NULL COMMENT '当事人签名',
+  `getter_sig` varchar(32) DEFAULT NULL COMMENT '收集人签名',
+  `material_path` varchar(300) DEFAULT NULL COMMENT '资料路径',
+  `material_comment` varchar(500) DEFAULT NULL COMMENT '资料说明',
+  `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='询问笔录项目';
+
+-- ----------------------------
+-- Table structure for tsignature
+-- ----------------------------
+DROP TABLE IF EXISTS `tsignature`;
+CREATE TABLE `tsignature` (
+  `id` varchar(64) NOT NULL COMMENT 'uuid',
+  `signature` blob NOT NULL COMMENT '签名',
+  `create_by` varchar(32) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(32) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(64) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='询问笔录项目';
